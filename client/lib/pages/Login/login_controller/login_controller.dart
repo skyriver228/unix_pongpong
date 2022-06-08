75dbbc0c0b0131b1aa3d95cfbde9e61efd5b9ff3
EungJae Lee <84191599+afkLee@users.noreply.github.com>
2022-06-09 03:04:28 +0900

    Add login_view_page

---
+++ client/lib/pages/Login/login_controller/login_controller.dart
@@ -0,0 +1,62 @@
+import 'package:client/common.dart';
+import 'package:client/pages/Login/login_controller/email_format_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginContoller extends GetxController {
+  final isEmailValid = true.obs;
+  final isPwdValid = true.obs;
+  final emailWarning = ''.obs;
+  final pwdWarning = ''.obs;
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController pwdController = TextEditingController();
+
+  void emailControllerListener() {
+    if (!EmailFormatHelper.isEmailValid(emailController.text.trim()) || emailController.text.isEmpty) {
+      emailWarning.value = '올바른 이메일 형식이 아닙니다.';
+      isEmailValid.value = false;
+    } else {
+      emailWarning.value = '';
+      isEmailValid.value = true;
+    }
+  }
+
+  void pwdControllerListener() {
+    if (pwdController.text.isEmpty) {
+      pwdWarning.value = '비밀번호를 입력해주세요';
+      isPwdValid.value = false;
+
+      return;
+    }
+
+    if (pwdController.text.length < 8) {
+      pwdWarning.value = '8자리 이상의 비밀번호를 입력해주세요';
+      isPwdValid.value = false;
+
+      return;
+    }
+
+    if (pwdController.text.isNotEmpty != pwdController.text.trim()) {
+      pwdWarning.value = '비밀번호가 일치하지 않습니다';
+      isPwdValid.value = false;
+
+      return;
+    }
+
+    pwdWarning.value = '';
+    isPwdValid.value = true;
+  }
+
+  bool verifyStep() {
+    emailControllerListener();
+    pwdControllerListener();
+
+    if (isEmailValid.value) {}
+    if (emailWarning.value != '') {
+      Common.CommonSnackBar(messageText: emailWarning.value);
+    }
+
+    return (isPwdValid.value && isEmailValid.value && EmailFormatHelper.isEmailValid(emailController.text.trim()));
+  }
+}
---
+++ client/lib/pages/Login/view/login_view_page2.dart
@@ -0,0 +1,137 @@
+import 'package:client/pages/Login/login_controller/login_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginViewPage2 extends StatelessWidget {
+  const LoginViewPage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final LoginContoller contoller = Get.put(LoginContoller());
+    // final googleLoginController = Get.put(GoogleLoginController());
+    // final kakaoLoginController = Get.put(KakaoLoginController());
+    return DefaultTextHeightBehavior(
+      textHeightBehavior: const TextHeightBehavior(leadingDistribution: TextLeadingDistribution.even),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: const Color(0xFF242731),
+        body: Stack(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Opacity(
+                    opacity: 0.6,
+                    child: Container(
+                      color: const Color(0xFF111111),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/Draft.png',
+                      width: 320,
+                      height: 150,
+                    ),
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Expanded(
+                      child: Opacity(
+                        opacity: 1,
+                        child: Container(
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: const Color(0xFF242731)),
+                          width: 300,
+                          height: 270,
+                          child: Center(
+                            child: Column(
+                              children: [
+                                const Padding(
+                                    padding: EdgeInsets.only(top: 30),
+                                    child: Text(
+                                      'Log in',
+                                      style: TextStyle(fontFamily: 'Poppins', color: Color(0xFFFAFAFB), fontWeight: FontWeight.w600, fontSize: 18),
+                                    )),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 30),
+                                  child: Container(
+                                    decoration: BoxDecoration(color: const Color(0xFF42424A), borderRadius: BorderRadius.circular(20)),
+                                    width: 260,
+                                    height: 38,
+                                    child: const TextField(
+                                      textAlign: TextAlign.start,
+                                      keyboardType: TextInputType.emailAddress,
+                                      style: TextStyle(fontFamily: 'Roboto', color: Color(0xFF92929D), fontWeight: FontWeight.w600, fontSize: 14),
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.transparent,
+                                          filled: true,
+                                          border: InputBorder.none,
+                                          contentPadding: EdgeInsets.only(left: 15, top: 5, bottom: 15, right: 24),
+                                          hintText: 'Your email',
+                                          hintStyle: TextStyle(fontFamily: 'Roboto', color: Color(0xFF92929D), fontWeight: FontWeight.w600, fontSize: 14)),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10),
+                                  child: Container(
+                                    decoration: BoxDecoration(color: const Color(0xFF42424A), borderRadius: BorderRadius.circular(20)),
+                                    width: 260,
+                                    height: 38,
+                                    child: const TextField(
+                                      obscureText: true,
+                                      textAlign: TextAlign.start,
+                                      keyboardType: TextInputType.emailAddress,
+                                      style: TextStyle(fontFamily: 'Roboto', color: Color(0xFF92929D), fontWeight: FontWeight.w600, fontSize: 14),
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.transparent,
+                                          filled: true,
+                                          border: InputBorder.none,
+                                          contentPadding: EdgeInsets.only(left: 15, top: 5, bottom: 15, right: 24),
+                                          hintText: 'Your Password',
+                                          hintStyle: TextStyle(fontFamily: 'Roboto', color: Color(0xFF92929D), fontWeight: FontWeight.w600, fontSize: 14)),
+                                    ),
+                                  ),
+                                ),
+                                Opacity(
+                                  opacity: 0.1,
+                                  child: Container(
+                                    margin: const EdgeInsets.only(top: 20, bottom: 20),
+                                    width: 240,
+                                    height: 2,
+                                    color: const Color(0xFFE4E4E4),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 270,
+                                  height: 40,
+                                  child: InkWell(
+                                    onTap: () {},
+                                    child: Image.network('https://www.figma.com/file/gNznrSMLdDRU0qhggi3DFM/Project_UNIX)-Draft?node-id=4%3A15150'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
